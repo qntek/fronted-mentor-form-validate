91273e62376a8b2fae373740be74044d3bdad081
Tomek K <tomasz.kuncio@gmail.com>
2022-12-04 16:51:28 +0100

    Name validation done

--- js/main.js
+++ js/main.js
@@ -2,12 +2,15 @@ const confirmBtn = document.getElementById('confirm-btn');
 const numberError = document.querySelector('.error-cardnumber');
 const inputNumberField = document.getElementById('cardnumber');
 const cardNumberOnCardImage = document.querySelector('.card-front-number');
+const cardHolderName = document.getElementById('cardholder-name');
 
 confirmBtn.addEventListener('click', confirmBtnHandler);
 inputNumberField.addEventListener('input', liveNumberChange);
+cardHolderName.addEventListener('input', liveNameChange);
 
 function confirmBtnHandler() {
 	numberValidator();
+	nameFieldValidate();
 }
 
 function numberValidator() {
@@ -23,7 +26,7 @@ function numberValidator() {
 }
 
 function liveNumberChange() {
-	inputNumberField.value = inputNumberField.value.replace(/\D/g,'');
+	inputNumberField.value = inputNumberField.value.replace(/\D/g, '');
 
 	if (inputNumberField.classList.contains('error-border')) {
 		inputNumberField.classList.remove('error-border');
@@ -32,10 +35,6 @@ function liveNumberChange() {
 
 	let currentValue = inputNumberField.value.padEnd(16, '0');
 
-	if (currentValue.length > 16) {
-		currentValue = currentValue.slice(0, 16);
-        numberValidator();
-	}
 	cardNumberOnCardImage.textContent = `${currentValue.slice(
 		0,
 		4
@@ -43,4 +42,22 @@ function liveNumberChange() {
 		8,
 		12
 	)} ${currentValue.slice(12, 16)}`;
+}
+
+function liveNameChange() {
+	const nameOnCardImage = document.querySelector('.card-front-cardholder-name');
+	nameOnCardImage.textContent = cardHolderName.value.toUpperCase();
+}
+
+function nameFieldValidate() {
+	const regName = /^[a-zA-Z]+ [a-zA-Z]+$/;
+	const name = cardHolderName.value;
+	const nameError = document.querySelector('.error-cardholder');
+	if (!regName.test(name)) {
+		nameError.classList.remove('off');
+		cardHolderName.classList.add('error-border');
+	} else {
+		nameError.classList.add('off');
+		cardHolderName.classList.remove('error-border');
+	}
 }
